62f4e6c54f5724afc7cb6f310b82e96e2bdfe5b8
eastwesttrading <eastweasttradingplc@gmail.com>
2026-07-29 02:07:52 +0300

    Add Firebase configuration to Firebase.js
    Added Firebase configuration with API key and project details.

--- Firebase.js
+++ Firebase.js
@@ -2,7 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  // Paste your Firebase configuration here
+  // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAC21sHOL7pIZi5044tNlu0hlt1rYlAqVg",
+  authDomain: "east-west-trading--plc.firebaseapp.com",
+  projectId: "east-west-trading--plc",
+  storageBucket: "east-west-trading--plc.firebasestorage.app",
+  messagingSenderId: "118174778678",
+  appId: "1:118174778678:web:44cab0c573dc1c6f9e1f97",
+  measurementId: "G-QVQCLT0JB3"
+};
 };
 
 const app = initializeApp(firebaseConfig);
