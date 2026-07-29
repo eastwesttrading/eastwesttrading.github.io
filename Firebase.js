af9f8647a9e39b6269c950920e3135cc3ab99b70
eastwesttrading <eastweasttradingplc@gmail.com>
2026-07-29 10:20:57 +0300

    Fix formatting and remove redundant comment in Firebase.js
    Fix formatting

--- Firebase.js
+++ Firebase.js
@@ -1,8 +1,7 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAC21sHOL7pIZi5044tNlu0hlt1rYlAqVg",
   authDomain: "east-west-trading--plc.firebaseapp.com",
@@ -11,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: "118174778678",
   appId: "1:118174778678:web:44cab0c573dc1c6f9e1f97",
   measurementId: "G-QVQCLT0JB3"
-};
 };
 
 const app = initializeApp(firebaseConfig);
