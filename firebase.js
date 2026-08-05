c264e8a81222224ff9defb0f1c9be0e92597470d
eastwesttrading <eastweasttradingplc@gmail.com>
2026-08-05 22:36:48 +0300

    Update firebase.js
    Update firebase

--- firebase.js
+++ firebase.js
@@ -1,19 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAC21sHOL7pIZi5044tNlu0hlt1rYlAqVg",
-  authDomain: "east-west-trading--plc.firebaseapp.com",
-  databaseURL: "https://east-west-trading--plc-default-rtdb.firebaseio.com",
-  projectId: "east-west-trading--plc",
-  storageBucket: "east-west-trading--plc.firebasestorage.app",
-  messagingSenderId: "118174778678",
-  appId: "1:118174778678:web:44cab0c573dc1c6f9e1f97",
-  measurementId: "G-QVQCLT0JB3"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
 export const auth = getAuth(app);
