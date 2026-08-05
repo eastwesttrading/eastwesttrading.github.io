70a6839a8bc5c6bdc5baa20b1f76a4ec5cf4cb0c
eastwesttrading <eastweasttradingplc@gmail.com>
2026-08-05 20:31:51 +0300

    Refactor firebase.js to use modular SDK
    Firebae.js to use modular

--- firebase.js
+++ firebase.js
@@ -1,12 +1,19 @@
-rules_version = '2';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-service cloud.firestore {
-  match /databases/{database}/documents {
+const firebaseConfig = {
+  apiKey: "AIzaSyAC21sHOL7pIZi5044tNlu0hlt1rYlAqVg",
+  authDomain: "east-west-trading--plc.firebaseapp.com",
+  databaseURL: "https://east-west-trading--plc-default-rtdb.firebaseio.com",
+  projectId: "east-west-trading--plc",
+  storageBucket: "east-west-trading--plc.firebasestorage.app",
+  messagingSenderId: "118174778678",
+  appId: "1:118174778678:web:44cab0c573dc1c6f9e1f97",
+  measurementId: "G-QVQCLT0JB3"
+};
 
-    match /{document=**} {
-      allow read: if true;
-      allow write: if request.auth != null;
-    }
+const app = initializeApp(firebaseConfig);
 
-  }
-}
+export const db = getFirestore(app);
+export const auth = getAuth(app);
