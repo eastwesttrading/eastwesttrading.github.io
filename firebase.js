4af49ff4084b442f9540513333a19121d356843f
eastwesttrading <eastweasttradingplc@gmail.com>
2026-08-15 22:44:44 +0300

    Modify Firebase authDomain value
    Updated authDomain in Firebase configuration.

--- firebase.js
+++ firebase.js
@@ -46,16 +46,16 @@ import {
 // ============================================================
 // 1. FIREBASE PROJECT CONFIGURATION
 // ============================================================
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAC21sHOL7pIZi5044tNlu0hlt1rYlAqVg",
-    authDomain: "east-west-trading--plc.firebaseapp.com",
-    databaseURL: "https://east-west-trading--plc-default-rtdb.firebaseio.com",
-    projectId: "east-west-trading--plc",
-    storageBucket: "east-west-trading--plc.firebasestorage.app",
-    messagingSenderId: "118174778678",
-    appId: "1:118174778678:web:44cab0c573dc1c6f9e1f97",
-    measurementId: "G-QVQCLT0JB3"
+  apiKey: "AIzaSyAC21sHOL7pIZi5044tNlu0hlt1rYlAqVg",
+  authDomain: "eastwest.com.et firebaseapp.com",
+  databaseURL: "https://east-west-trading--plc-default-rtdb.firebaseio.com",
+  projectId: "east-west-trading--plc",
+  storageBucket: "east-west-trading--plc.firebasestorage.app",
+  messagingSenderId: "118174778678",
+  appId: "1:118174778678:web:44cab0c573dc1c6f9e1f97",
+  measurementId: "G-QVQCLT0JB3"
 };
 
 
